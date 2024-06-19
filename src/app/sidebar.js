17de8e16fc4680ef7e527fb80a47793ece3508a3
Jason Zheng <jzh05@students.cs.ubc.ca>
2024-06-18 18:18:44 -0700

    Initial commit

---
+++ src/app/sidebar.js
@@ -0,0 +1,9 @@
+
+export default function sidebar() {
+    return (
+        <div className="fixed top-12 left-12 flex flex-col p-8 bg-slate-800 rounded-3xl shadow-xl transition ease-in-out delay-130 hover:bg-slate-700">
+            <h1 className="text-3xl text-white font-Bebas_Neue text-opacity-45">Hi I&apos;m</h1>
+            <h1 className="text-9xl font-Bebas_Neue underline decoration-teal-700/55">Jason Zheng</h1>
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { Bebas_Neue } = require('next/font/google');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,6 +14,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        Bebas_Neue: 'Bebas Neue, sans-serif',
+      }
     },
   },
   plugins: [],
